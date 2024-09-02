a3a9d24c8fa58815995d00058e414a918e411529
Lenovo <mijaeljuy@tecsup.edu.pe>
2024-09-01 21:40:59 -0500

    OutlinedTextField

--- app/src/main/java/com/example/lab_4/MainActivity.kt
+++ app/src/main/java/com/example/lab_4/MainActivity.kt
@@ -4,18 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab_4.ui.theme.Lab_4Theme
 
 class MainActivity : ComponentActivity() {
@@ -44,12 +43,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    LazyColumn(
+    var text by remember { mutableStateOf("") }
+
+    Column(
         modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        items(10) { index ->
-            Text(text = "Item #$index")
+        LazyColumn(
+            modifier = Modifier.weight(1f)
+        ) {
+            items(10) { index ->
+                Text(text = "Item #$index")
+            }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Enter text") }
+        )
     }
 }
 
